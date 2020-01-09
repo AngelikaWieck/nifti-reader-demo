c71d8071d79d58a36036385b64943e51138455fb
Angelika Wieck <angelika.wieck@student.hpi.de>
2020-01-10 00:02:22 +0100

    little refactoring

--- index.js
+++ index.js
@@ -1,175 +1,217 @@
-function readNIFTI(name, data) {
-  let slider = document.getElementById("myRange");
-
-  let niftiHeader, niftiImage;
-  // parse nifti
-  if (nifti.isCompressed(data)) {
-    data = nifti.decompress(data);
-  }
-  if (nifti.isNIFTI(data)) {
-    niftiHeader = nifti.readHeader(data);
-    niftiImage = nifti.readImage(niftiHeader, data);
-  }
-
-  let typedData;
-  // convert raw data to typed array based on nifti datatype
-  if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_UINT8) {
-    typedData = new Uint8Array(niftiImage);
-  } else if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_INT16) {
-    typedData = new Int16Array(niftiImage);
-  } else if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_INT32) {
-    typedData = new Int32Array(niftiImage);
-  } else if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_FLOAT32) {
-    typedData = new Float32Array(niftiImage);
-  } else if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_FLOAT64) {
-    typedData = new Float64Array(niftiImage);
-  } else if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_INT8) {
-    typedData = new Int8Array(niftiImage);
-  } else if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_UINT16) {
-    typedData = new Uint16Array(niftiImage);
-  } else if (niftiHeader.datatypeCode === nifti.NIFTI1.TYPE_UINT32) {
-    typedData = new Uint32Array(niftiImage);
-  } else {
-    return;
-  }
-
-  let min = typedData[0];
-  let max = typedData[0];
-  for (let i = 0; i < typedData.length; i++) {
-    if (typedData[i] < min) min = typedData[i];
-    if (typedData[i] > max) max = typedData[i];
-  }
-
-  // convert to 8-bit representation that is required for rgba
-  let factor = 256 / (max - min);
-  for (let i = 0; i < typedData.length; i++) {
-    typedData[i] = (typedData[i] - min) * factor;
-  }
-
-  // set up slider
-  let slices = niftiHeader.dims[3];
-  slider.max = slices - 1;
-  slider.value = Math.round(slices / 2);
-  slider.oninput = function() {
-    drawCanvasXY(slider.value | 0, slices, niftiHeader, typedData);
-    drawCanvasYZ(slider.value | 0, slices, niftiHeader, typedData);
-    drawCanvasXZ(slider.value | 0, slices, niftiHeader, typedData);
-  };
-  // draw slice
-  drawCanvasXY(slider.value | 0, slices, niftiHeader, typedData);
-  drawCanvasYZ(slider.value | 0, slices, niftiHeader, typedData);
-  drawCanvasXZ(slider.value | 0, slices, niftiHeader, typedData);
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = evt => {
+      resolve(reader.result);
+    };
+    reader.onerror = evt => {
+      reject(reader.error);
+    };
+    reader.readAsArrayBuffer(file);
+  });
 }
 
-function drawCanvasXY(slice, slices, niftiHeader, typedData) {
-  let canvas = document.getElementById("canvasXY");
-
-  let cols = niftiHeader.dims[1];
-  let rows = niftiHeader.dims[2];
-
-  // set canvas dimensions to nifti slice dimensions
-  canvas.width = cols;
-  canvas.height = rows;
-
-  // make canvas image data
-  let ctx = canvas.getContext("2d");
-  let canvasImageData = ctx.createImageData(canvas.width, canvas.height);
-
-  // offset to specified slice
-  let sliceSize = cols * rows;
-  let sliceOffset = sliceSize * slice;
-
-  let canvasImageDataIndex = 0;
-  for (let i = sliceOffset; i < sliceOffset + sliceSize; i++) {
-    canvasImageData.data[canvasImageDataIndex] = typedData[i]; // r
-    canvasImageData.data[canvasImageDataIndex + 1] = typedData[i]; // g
-    canvasImageData.data[canvasImageDataIndex + 2] = typedData[i]; // b
-    canvasImageData.data[canvasImageDataIndex + 3] = 0xff; // a
-    canvasImageDataIndex += 4;
-  }
-  ctx.putImageData(canvasImageData, 0, 0);
-}
-
-function drawCanvasYZ(slice, slices, niftiHeader, typedData) {
-  // get nifti dimensions
-  let cols = niftiHeader.dims[1];
-  let rows = niftiHeader.dims[2];
-
-  let canvas = document.getElementById("canvasYZ");
-
-  // set canvas dimensions to nifti slice dimensions
-  canvas.width = niftiHeader.dims[2];
-  canvas.height = niftiHeader.dims[3];
-  // make canvas image data
-  let ctx = canvas.getContext("2d");
-
-  // width and height anpassen
-  let canvasImageData = ctx.createImageData(canvas.width, canvas.height);
-
-  // offset to specified slice
-  let sliceSize = cols * rows;
-  let sliceOffset = sliceSize * slice;
-
-  let canvasImageDataIndex = 0;
-  for (let i = slice; i < sliceSize * slices; i += cols) {
-    canvasImageData.data[canvasImageDataIndex] = typedData[i];
-    canvasImageData.data[canvasImageDataIndex + 1] = typedData[i];
-    canvasImageData.data[canvasImageDataIndex + 2] = typedData[i];
-    canvasImageData.data[canvasImageDataIndex + 3] = 0xff;
-    canvasImageDataIndex += 4;
+class NIFTIImage {
+  static async readFile(file) {
+    const data = await readFile(file);
+    return this.parseData(data);
   }
 
-  ctx.putImageData(canvasImageData, 0, 0);
+  static parseData(data) {
+    if (nifti.isCompressed(data)) {
+      data = nifti.decompress(data);
+    }
+    if (!nifti.isNIFTI(data)) {
+      throw new Error("This is not a NIFTI image!!");
+    }
+    let header = nifti.readHeader(data);
+    let image = nifti.readImage(header, data);
+    return new this(header, image);
+  }
+
+  constructor(header, image) {
+    this.header = header;
+    this.image = image;
+
+    let typedData;
+    // convert raw data to typed array based on nifti datatype
+    if (header.datatypeCode === nifti.NIFTI1.TYPE_UINT8) {
+      typedData = new Uint8Array(image);
+    } else if (header.datatypeCode === nifti.NIFTI1.TYPE_INT16) {
+      typedData = new Int16Array(image);
+    } else if (header.datatypeCode === nifti.NIFTI1.TYPE_INT32) {
+      typedData = new Int32Array(image);
+    } else if (header.datatypeCode === nifti.NIFTI1.TYPE_FLOAT32) {
+      typedData = new Float32Array(image);
+    } else if (header.datatypeCode === nifti.NIFTI1.TYPE_FLOAT64) {
+      typedData = new Float64Array(image);
+    } else if (header.datatypeCode === nifti.NIFTI1.TYPE_INT8) {
+      typedData = new Int8Array(image);
+    } else if (header.datatypeCode === nifti.NIFTI1.TYPE_UINT16) {
+      typedData = new Uint16Array(image);
+    } else if (header.datatypeCode === nifti.NIFTI1.TYPE_UINT32) {
+      typedData = new Uint32Array(image);
+    } else {
+      return;
+    }
+
+    let min = typedData[0];
+    let max = typedData[0];
+    for (let i = 0; i < typedData.length; i++) {
+      if (typedData[i] < min) min = typedData[i];
+      if (typedData[i] > max) max = typedData[i];
+    }
+
+    let factor = 256 / (max - min);
+    for (let i = 0; i < typedData.length; i++) {
+      typedData[i] = (typedData[i] - min) * factor;
+    }
+
+    this.data = typedData;
+  }
 }
 
-function drawCanvasXZ(slice, slices, niftiHeader, typedData) {
-  // get nifti dimensions
-  let cols = niftiHeader.dims[1];
-  let rows = niftiHeader.dims[2];
+class SliceView {
+  constructor(canvas, image) {
+    this.canvas = canvas;
+    this.image = null;
+    this.ctx = null;
+    if (image) {
+      this.setImage(image);
+    }
+  }
 
-  let canvas = document.getElementById("canvasXZ");
+  get dims() {
+    return this.image.header.dims;
+  }
 
-  // set canvas dimensions to nifti slice dimensions
-  canvas.width = niftiHeader.dims[1];
-  canvas.height = niftiHeader.dims[3];
-  // make canvas image data
-  let ctx = canvas.getContext("2d");
+  get cols() {
+    return this.dims[1];
+  }
 
-  // width and height anpassen
-  let canvasImageData = ctx.createImageData(canvas.width, canvas.height);
+  get rows() {
+    return this.dims[2];
+  }
 
-  // offset to specified slice
-  let sliceSize = cols * rows;
-  let sliceOffset = sliceSize * slice;
+  get slices() {
+    return this.dims[3];
+  }
 
-  let canvasImageDataIndex = 0;
-  for (let i = 0; i < sliceSize * slices; i += sliceSize) {
-    for (
-      let k = sliceSize - slice * cols;
-      k < sliceSize - slice * cols + cols;
-      k++
-    ) {
-      canvasImageData.data[canvasImageDataIndex] = typedData[k + i];
-      canvasImageData.data[canvasImageDataIndex + 1] = typedData[k + i];
-      canvasImageData.data[canvasImageDataIndex + 2] = typedData[k + i];
+  get canvasDim() {
+    throw new Error("No canvasDim getter, because this is the super class");
+  }
+
+  setImage(image) {
+    this.image = image;
+    // set canvas dimensions to nifti slice dimensions
+    const [width, height] = this.canvasDim;
+    this.canvas.width = width;
+    this.canvas.height = height;
+
+    // make canvas image data
+    this.ctx = this.canvas.getContext("2d");
+  }
+
+  innerUpdate() {
+    throw new Error("No innerUpdate function, because this is the super class");
+  }
+
+  update(slice) {
+    let canvasImageData = this.ctx.createImageData(
+      this.canvas.width,
+      this.canvas.height
+    );
+
+    this.innerUpdate(canvasImageData, slice);
+
+    this.ctx.putImageData(canvasImageData, 0, 0);
+  }
+}
+
+class SliceViewXY extends SliceView {
+  get canvasDim() {
+    return [this.cols, this.rows];
+  }
+
+  innerUpdate(canvasImageData, slice) {
+    let sliceSize = this.cols * this.rows;
+    let sliceOffset = sliceSize * slice;
+
+    let canvasImageDataIndex = 0;
+    for (let i = sliceOffset; i < sliceOffset + sliceSize; i++) {
+      canvasImageData.data[canvasImageDataIndex] = this.image.data[i]; // r
+      canvasImageData.data[canvasImageDataIndex + 1] = this.image.data[i]; // g
+      canvasImageData.data[canvasImageDataIndex + 2] = this.image.data[i]; // b
+      canvasImageData.data[canvasImageDataIndex + 3] = 0xff; // a
+      canvasImageDataIndex += 4;
+    }
+  }
+}
+
+class SliceViewYZ extends SliceView {
+  get canvasDim() {
+    return [this.rows, this.slices];
+  }
+
+  innerUpdate(canvasImageData, slice) {
+    let sliceSize = this.cols * this.rows;
+
+    let canvasImageDataIndex = 0;
+    for (let i = slice; i < sliceSize * this.slices; i += this.cols) {
+      canvasImageData.data[canvasImageDataIndex] = this.image.data[i];
+      canvasImageData.data[canvasImageDataIndex + 1] = this.image.data[i];
+      canvasImageData.data[canvasImageDataIndex + 2] = this.image.data[i];
       canvasImageData.data[canvasImageDataIndex + 3] = 0xff;
       canvasImageDataIndex += 4;
     }
   }
-
-  ctx.putImageData(canvasImageData, 0, 0);
 }
 
-function readFile(file) {
-  let reader = new FileReader();
-  reader.onloadend = function(evt) {
-    if (evt.target.readyState === FileReader.DONE) {
-      readNIFTI(file.name, evt.target.result);
+class SliceViewXZ extends SliceView {
+  get canvasDim() {
+    return [this.cols, this.slices];
+  }
+
+  innerUpdate(canvasImageData, slice) {
+    let sliceSize = this.cols * this.rows;
+
+    let canvasImageDataIndex = 0;
+    for (let i = 0; i < sliceSize * this.slices; i += sliceSize) {
+      for (
+        let k = sliceSize - slice * this.cols;
+        k < sliceSize - slice * this.cols + this.cols;
+        k++
+      ) {
+        canvasImageData.data[canvasImageDataIndex] = this.image.data[k + i];
+        canvasImageData.data[canvasImageDataIndex + 1] = this.image.data[k + i];
+        canvasImageData.data[canvasImageDataIndex + 2] = this.image.data[k + i];
+        canvasImageData.data[canvasImageDataIndex + 3] = 0xff;
+        canvasImageDataIndex += 4;
+      }
     }
-  };
-  reader.readAsArrayBuffer(file);
+  }
 }
-function handleFileSelect(evt) {
-  let files = evt.target.files;
-  readFile(files[0]);
+
+async function handleFileSelect(event) {
+  let slider = document.getElementById("myRange");
+
+  const image = await NIFTIImage.readFile(event.target.files[0]);
+
+  let sliceViewXY = new SliceViewXY(document.getElementById("canvasXY"), image);
+  let sliceViewYZ = new SliceViewYZ(document.getElementById("canvasYZ"), image);
+  let sliceViewXZ = new SliceViewXZ(document.getElementById("canvasXZ"), image);
+
+  // set up slider
+  let slices = image.header.dims[3];
+  slider.max = slices - 1;
+  slider.value = Math.round(slices / 2);
+  slider.oninput = function() {
+    sliceViewXY.update(slider.value | 0);
+    sliceViewYZ.update(slider.value | 0);
+    sliceViewXZ.update(slider.value | 0);
+  };
+  // draw slice
+  sliceViewXY.update(slider.value | 0);
+  sliceViewYZ.update(slider.value | 0);
+  sliceViewXZ.update(slider.value | 0);
 }
